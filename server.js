3ebfc55d01a2fc7fa977f251bc7151d2a4ac7f7c
Artur <dilmukhametovar@gmail.com>
2020-01-10 19:47:58 +0300

    index page

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const app = express();
 const HOST = '127.0.0.1';
 const PORT = 8080;
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-	res.send('Hello, world!');
+	res.render('index');
 });
 
 app.listen(PORT, HOST, () => {
